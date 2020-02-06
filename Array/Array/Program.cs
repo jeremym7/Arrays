826372daca93a5757fc13d2c06ee2b86bc0863cd
veroj_000 <veroj_000@JEREMY-LAPTOP>
2020-02-06 17:11:20 -0500

    commit comments

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -6,8 +6,10 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            //Generates the array
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
 
+            //Lists each element in the array to the user with a foreach loop
             foreach (int i in numbers) 
             {
                 Console.WriteLine("Element value" + " = " + i + " ");        
